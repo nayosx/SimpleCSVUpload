709b3c2a9420b80e14f6dc614d954a5c1a93c026
nayosx <nayos144@gmail.com>
2021-08-11 22:50:39 -0600

    added first commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # SimpleCSVUpload
 A simple project for upload any CSV write in node
+
+This project use 
+
+- express
+- multer
+- cors
+- nodemon
+
+First is create a directory "uploads"
+
+## nodemon is installed for global env
+
+```
+npm install -g nodemon
+```
+
+## for run
+
+```
+nodemon dev
+```
+
---
+++ app.js
@@ -0,0 +1,60 @@
+const multer = require('multer');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+global.__basedir = __dirname;
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, __basedir + '/uploads/');
+    },
+    filename: (req, file, callback) => {
+        callback(null, file.fieldname + "-" + Date.now() + "-" + file.originalname);
+    }
+});
+
+const filterCSV = (req, file, callback) => {
+    let ext = path.extname(file.originalname);
+    if(ext === '.csv') {
+        callback(null, true);
+    } else {
+        callback('File format is not a CSV', false);
+    }
+}
+
+const upload = multer({
+    storage: storage,
+    fileFilter: filterCSV
+});
+
+app.use(cors())
+
+app.post('/', upload.single('uploadFile'), (req, res) => {
+    try{
+        if(req.file == undefined) {
+            res.status(400).send({
+                message: "Please upload a CSV file: "
+            });
+        } else {
+            res.status(200).send({
+                message: "File Upload: "
+            });
+        }
+    } catch(error) {
+        res.status(500).send({
+            message: "Could not upload the file: " + req.file.originalname
+        });
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send("Hello from Node") ;
+});
+
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`App listening port ${port}`);
+});
